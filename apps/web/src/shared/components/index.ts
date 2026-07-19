ea76320d351479d660ca3e67287b2bb0dcd7219d
TIRTH <tktirth2006@gmail.com>
2026-07-19 12:08:18 +0530

    style(shared-index): export all v2 visual language components from barrel file

--- apps/web/src/shared/components/index.ts
+++ apps/web/src/shared/components/index.ts
@@ -10,3 +10,12 @@ export * from './ErrorState';
 export * from "./3d/Scene";
 export * from "./3d/HoloBrain";
 export * from "./3d/ParticleField";
+
+// v2 visual language components
+export * from './HudCornerFrame';
+export * from './ClinicalMetaTable';
+export * from './ModelSelectorSegmented';
+export * from './InspectorPopoutCard';
+export * from './FrequencyBandRow';
+export * from './SessionFilmstrip';
+export * from './DualLineComparisonChart';
